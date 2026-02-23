6b1b8d2bbc06d0ebdabf6ce10e510214c53df849
DakshGhai <dg0507@srmist.edu.in>
2026-02-23 10:11:42 +0530

    Coded UC2 to Print OOPS to Console

--- Banner.java
+++ Banner.java
@@ -1,7 +1,13 @@
 public class Banner {
-
-    public static void main(String[] args) {
-
-        System.out.println("OOPS");
+    public static void main(String[] var0) {
+        System.out.println("  *****    *****    *****     *****   ");
+        System.out.println(" *     *  *     *  *     *   *     *  ");
+        System.out.println(" *     *  *     *  *     *   *     *  ");
+        System.out.println(" *     *  *     *  *     *   *        ");
+        System.out.println(" *     *  *     *   *****     *****   ");
+        System.out.println(" *     *  *     *  *               *  ");
+        System.out.println(" *     *  *     *  *         *     *  ");
+        System.out.println(" *     *  *     *  *         *     *  ");
+        System.out.println("  *****    *****   *          *****   ");
     }
 }
