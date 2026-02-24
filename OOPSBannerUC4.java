7986b8b95786258a35a3c400996f5a35ccf72d3a
DakshGhai <dg0507@srmist.edu.in>
2026-02-24 13:17:05 +0530

    Coded UC5 to Print OOPS to Console

--- OOPSBannerUC4.java
+++ OOPSBannerUC4.java
@@ -1,7 +1,8 @@
 public class OOPSBannerUC4 {
 
-    public static String[] createBanner() {
+    public static void main(String[] args) {
 
+        
         String[] banner = {
             String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
             String.join("   ", "*     *", "*     *", "*     *", "*      "),
@@ -10,21 +11,9 @@ public class OOPSBannerUC4 {
             String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
         };
 
-        return banner;
-    }
-
-        public static void printBanner(String[] banner) {
+        
         for (String line : banner) {
             System.out.println(line);
         }
-    }
-
-    public static void main(String[] args) {
-
-       
-        String[] banner = createBanner();
-
-       
-        printBanner(banner);
     }
 }
